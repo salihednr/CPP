0794a80aae33318cfdc838e2613483e1252bb57c
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-25 22:10:41 +0530

    Update arrays.cpp
    added some concepts

--- 00_CONCEPTS/arrays.cpp
+++ 00_CONCEPTS/arrays.cpp
@@ -1,10 +1,15 @@
-/*
-Array's Concept
-*/
-#include<iostream>
+
+#include <iostream>
 using namespace std;
-int main()
-{
-  
-  return 0;
+
+int main() {
+	// your code goes here
+	return 0;
 }
+/*
+1) Array Starts From [0,N-1]
+2) Arrays Must Be Same Type In C++,JAVA,C And They Are Stored In Contiguos Memery Location
+3) If Arrays Is Not Initialised Then It Will Contain Garbage Values
+4) If arrays is initialised except initilised values all other are assigned as 0
+5) String And Character Arrays Must Contain Null Character '\0'
+*/

1387be1563a7f5929adc2f67c70903b4c8b3a7d6
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-25 22:12:37 +0530

    Update arrays.cpp

--- 00_CONCEPTS/arrays.cpp
+++ 00_CONCEPTS/arrays.cpp
@@ -12,4 +12,5 @@ int main() {
 3) If Arrays Is Not Initialised Then It Will Contain Garbage Values
 4) If arrays is initialised except initilised values all other are assigned as 0
 5) String And Character Arrays Must Contain Null Character '\0'
+6) Size Is Optional Even Tough Once It is initialised we can't change size it will take size as how many elements it is containing
 */

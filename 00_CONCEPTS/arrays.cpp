f1c7f0a1f1c02c7794375da7a39f90b6b655e91f
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-25 22:26:38 +0530

    Update arrays.cpp

--- 00_CONCEPTS/arrays.cpp
+++ 00_CONCEPTS/arrays.cpp
@@ -9,8 +9,8 @@ int main() {
 /*
 1) Array Starts From [0,N-1]
 2) Arrays Must Be Same Type In C++,JAVA,C And They Are Stored In Contiguos Memery Location
-3) If Arrays Is Not Initialised Then It Will Contain Garbage Values
-4) If arrays is initialised except initilised values all other are assigned as 0
+3) If Arrays Is Not Initialised Then It Will Contain Garbage Values 
+4) If arrays is initialised except initilised values all other are assigned as 0 It must be initilised not updating (eg inputting using for loop)
 5) String And Character Arrays Must Contain Null Character '\0'
 6) Size Is Optional Even Tough Once It is initialised we can't change size it will take size as how many elements it is containing
 */

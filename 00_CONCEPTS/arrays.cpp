78f781c9097a89830d5b6b80d36205ff46607adf
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-27 17:02:23 +0530

    Update arrays.cpp

--- 00_CONCEPTS/arrays.cpp
+++ 00_CONCEPTS/arrays.cpp
@@ -12,7 +12,11 @@ int main() {
 3) If Arrays Is Not Initialised Then It Will Contain Garbage Values 
 4) If arrays is initialised except initilised values all other are assigned as 0 It must be initilised not updating (eg inputting using for loop)
 5) String And Character Arrays Must Contain Null Character '\0'
-6) Size Is Optional Even Tough Once It is initialised we can't change size it will take size as how many elements it is containing
+6) Size Is Optional Even Tough Once It is initialised we can't change size it will take size as how many elements it is containing if we are not specifing size
 7) capacity of int/char/double array = sizeof(array)/sizeof(int)
-8) When we are passing an array we should pass size(capacity) of array as well Because Array inbuilt is pass by referance
+8) When we are passing an array we should pass size(capacity) of array as well Because Array inbuilt is pass by referance so variable only storing address of array
+9) array[i] this is not pass by referance this is just similar to passing the elements
+10) If a[] is an array then cout<<a and cout<<&a will only print same memory location by default (printing with just variable name without[]) it will print it's
+memory location only
+11) arrayCall(ar) -> arrayCall(int ar) which is equivalent to arrayCall(&ar) -> arrayCall(int *ar)
 */

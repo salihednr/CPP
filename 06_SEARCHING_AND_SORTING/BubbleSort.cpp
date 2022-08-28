ad55e6337bf660c9d9f74710724c7592dba0decf
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-28 23:50:45 +0530

    Create BubbleSort.cpp
    added bubble sort

---
+++ 06_SEARCHING_AND_SORTING/BubbleSort.cpp
@@ -0,0 +1,44 @@
+/*
+Sorting :- Arranging In The Order Of Increasing/Decreasing Order
+Bubble Sort:- Take Larger Element To The End By Repeatedly Swapping The Adjascent Element
+*/
+#include<iostream>
+using namespace std;
+void bubbleSort(int ar[],int size);
+int main()
+{
+  int n;
+  cin>>n;
+  int ar[n];
+  for(int i=0;i<n;i++)
+  cin>>ar[i];
+  bubbleSort(ar,n);
+  for(int i=0;i<n;i++)
+  cout<<ar[i]<<" ";
+  return 0;
+}
+void bubbleSort(int ar[],int size)
+{
+  for(int times=1;times<=size-1;times++)
+  {
+    int flag=0;
+    //This Loop(Second for loop) Will Work C(N,2) Times without using break and flag using flag will help us to stop loops if array is already sorted reduce steps;
+    for(int i=0;i<=size-times-1;i++)
+    {
+        if(ar[i]>ar[i+1])
+        {
+          swap(ar[i],ar[i+1]); //inbuilt keyword to swap
+          flag=1;
+        }
+        cout<<"Checking"<<endl;
+    }
+    if(flag==0)
+      break;
+  }
+}
+/*
+size =5;
+1 2 
+1) 
+
+*/

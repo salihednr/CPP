4d8668e4012c8112092f17d77a622376f24994db
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-08-30 10:32:18 +0530

    Update flattening array.cpp
    added comments for better understanding

--- 03_ARRAYS/flattening array.cpp
+++ 03_ARRAYS/flattening array.cpp
@@ -9,14 +9,17 @@ int main() {
 	cin>>m>>n;
 	int arrayTwoD[m][n];
 	int arrayOneD[m*n];
+	//taking input
 	for(int i=0;i<m;i++)
 		for(int j=0;j<n;j++)
 			cin>>arrayTwoD[i][j];
+	//Flattening 2D Array To One D Array (Storing 2D Array Values In 1D Array)
 	for(int i=0;i<m;i++)
 		for(int j=0;j<n;j++)
 			{
 				arrayOneD[(i*n)+j]=arrayTwoD[i][j];
 			}
+	//printing array
 	for(int i=0;i<(m*n);i++)
 	cout<<arrayOneD[i];
 	return 0;

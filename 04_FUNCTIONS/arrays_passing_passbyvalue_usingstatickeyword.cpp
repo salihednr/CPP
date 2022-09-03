12473c989cb1beaa23f9328ca4ead24dc42299ee
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-09-04 00:12:41 +0530

    Update and rename arraypassbyvalueandstatickeyword.cpp to arrays_passing_passbyvalue_usingstatickeyword.cpp

--- 04_FUNCTIONS/arrays_passing_passbyvalue_usingstatickeyword.cpp
+++ 04_FUNCTIONS/arrays_passing_passbyvalue_usingstatickeyword.cpp
@@ -1,6 +1,6 @@
 /*
 Passing Array Via Pass By Value Using Static Keyword
-Size Is Know Then Only Possible
+Size Must Be Given Then Only Possible
 */
 #include<iostream>
 using namespace std;
@@ -9,6 +9,7 @@ void ar(int n,int i)
 {
     static int numbers[3];
     numbers[i]=n;
+    // this will check array is completed so we can print
     if(i==(len-1))
     {
         for(int i=0;i<len;i++)

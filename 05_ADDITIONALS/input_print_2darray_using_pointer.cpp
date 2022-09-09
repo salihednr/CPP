5003a1096df1e372e70f7a6045710baaa0c1b04f
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-09-09 15:01:32 +0530

    Update input_print_2darray_using_pointer.cpp

--- 05_ADDITIONALS/input_print_2darray_using_pointer.cpp
+++ 05_ADDITIONALS/input_print_2darray_using_pointer.cpp
@@ -11,7 +11,7 @@ int main() {
 	int n,m;
 	cin>>n>>m;
 	int a[n][m];
-	int *p=&a[0][0]; // int*p=&a[m] only column is mandatory (also while passing 2d array only coloumn is required first box(row) is always optional)
+	//int *p=&a[0][0]; // int*p=&a[m] only column is mandatory (also while passing 2d array only coloumn is required first box(row) is always optional)
   for(int i=0;i<n;i++)
     for(int j=0;j<m;j++)
       cin>>*(*(a+i)+j);

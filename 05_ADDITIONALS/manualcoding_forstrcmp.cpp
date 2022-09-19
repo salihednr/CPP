71e7e5f61af6963abab98937d91f33c8679a5243
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-09-19 11:10:32 +0530

    Update manualcoding_forstrcmp.cpp
    added documentation

--- 05_ADDITIONALS/manualcoding_forstrcmp.cpp
+++ 05_ADDITIONALS/manualcoding_forstrcmp.cpp
@@ -1,6 +1,13 @@
 /*
 Principle Of 
 Inbuilt Function strcmp 
+How strcmp Works ?
+strcmp will either return 0 or non zero value if it is zero then they are of same type
+if it is non zero then they are of not same type
++ve means first string comes lower order(dictionary order) than second string
+-ve means second string comes higher order (dictionary order) than second string
+it just takes each letter and compare whether a[i]-b[i]==0 that is how it works if it is not zero
+then it returns a[i]-b[i]
 */
 #include<iostream>
 #include<cstring>
